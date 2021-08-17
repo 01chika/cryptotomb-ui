2de40f2b1dc19f6b87e3264f445f3262ff7ed839
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2021-08-17 04:02:19 -0400

    initialized && rendered gallery

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,8 +1,67 @@
+import { useEffect, useState } from "react";
+import Button from "./components/Button";
+import Plots from "./components/Plots";
+
 const Gallery = () => {
+    // const {data, isPending, error} = useFetch("http://localhost:8000/plots");
+    const [allData, setAllData] = useState(null);
+    const [isPending, setisPending] = useState(true);
+    const [error, setError] = useState(null);
+    const [plotsdata, setPlots] = useState(null);
+    const [buttons, setButtons] = useState(null);
+
+    useEffect(() => {
+        const abortCont = new AbortController();
+        setTimeout(() => {
+            fetch("http://localhost:8000/plots", { signal: abortCont.signal })
+            .then(res => {
+                console.log(res);
+                if(!res.ok){
+                    throw Error("could not fetch data");
+                }
+                return res.json();
+            })
+            .then(data => {
+                setPlots(data);
+                setAllData(data);
+                setisPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                if (err.name === "AbortError"){
+                    console.log("fetch aborted");
+                } else{
+                    setError(err.message);
+                    setisPending(false);
+                }
+                
+            })
+            
+        });
+
+        return () => abortCont.abort();
+
+    }, []);
+
+
+    const filter = (button) =>{
+        if(button === 'All'){
+            setPlots(allData);
+            return;
+        }
+
+        const filteredData = allData.filter(plot => plot.tombType === button);
+        setPlots(filteredData);
+    }
+
     return ( 
-        <div className="gallery-menu">
-            <h1>GALLERY MENU</h1>
-        </div>
+
+        <section className="gallery">
+            {error && <div className="fetch-error">could not fetch the data for that resource</div>}
+            {isPending && <h2> Loading...</h2> }
+            {allData && <Button data={allData} filter={filter}/>}
+            {plotsdata && <Plots plots={plotsdata}/> }
+        </section>
      );
 }
  

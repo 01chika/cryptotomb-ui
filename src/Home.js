04a862679e489ba8ffa54094a80c4a031115fcfc
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2021-08-17 04:02:35 -0400

    initialized homepage and added content

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,24 @@
+
 const Home = () => {
+
     return ( 
-        <div className="home">
-            <h1>HOME</h1>
-        </div>
+        <section className="home">
+            <div class="hero-section">
+                <div class="hero-1">
+                    <h1 class="hero-title">
+                        Immortalize your Identity on the Ethereum Blockchain forever<span className="dot">.</span>
+                    </h1>
+                    <p class="hero-paragraph">
+                        lorem ipsum Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, veniam unde? Nostrum sint ipsum aspernatur doloremque perferendis suscipit laborum optio delectus ab repudiandae alias natus expedita quod, ut quos quo.
+                    </p>
+                    <div className="claim-a-plot">Claim a Plot</div>
+
+                </div>
+                <div class="hero-illustration">
+                    <img src={"/eth-logo-black.png"} alt="ddd"/>
+                </div>
+            </div>
+        </section>
      );
 }
  

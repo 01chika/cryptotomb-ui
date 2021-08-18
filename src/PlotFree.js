854409527e1e551819b6c6269e39389c38c3b32e
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2021-08-18 01:35:52 -0400

    initialized plotFree component

---
+++ src/PlotFree.js
@@ -0,0 +1,10 @@
+const PlotFree = () => {
+    return ( 
+        <div>
+            Lucky You, this plot has not been claimed...Hurry and claim it now!!!
+                <div className="claim-this-plot">Claim this Plot</div>
+        </div>
+    );
+}
+ 
+export default PlotFree;

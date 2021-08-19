1e5ca347a3b75986269c7778be56e426dd2ba1b9
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2021-08-18 21:13:30 -0400

    refactored component to be more robust

--- src/PlotFree.js
+++ src/PlotFree.js
@@ -1,10 +1,26 @@
-const PlotFree = () => {
+const PlotFree = ({plot}) => {
     return ( 
-        <div>
-            Lucky You, this plot has not been claimed...Hurry and claim it now!!!
-                <div className="claim-this-plot">Claim this Plot</div>
-        </div>
+        <section id="free" className="plot-info-section">
+
+            {plot && <div className="plot-info-details">
+                <h1 ><span className="highlighted-plot">{`Plot #${plot.id}`}</span></h1>
+
+                <div className="plot-image">
+                    <img src={plot.image} alt="" />
+                </div>
+            </div>
+            }
+
+            {plot &&<div className="plot-claim-status">
+                <div>
+                    Lucky You, this plot has not been claimed...Hurry and claim it now!!!
+                        <div className="claim-this-plot">Claim this Plot</div>
+                </div>
+            </div>
+            }
+        </section>
     );
 }
  
 export default PlotFree;
+

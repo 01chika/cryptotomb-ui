b8336ea657ffd6a440dd177c535ea0249eef6096
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2021-08-19 21:47:52 -0400

    added 'plotClaimed' component

--- src/PlotDetail.js
+++ src/PlotDetail.js
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import PlotClaimed from "./PlotClaimed";
 import PlotFree from "./PlotFree";
 import useFetch from "./useFetch";
 
@@ -6,10 +7,15 @@ const PlotDetail = () => {
 
     const { id } = useParams();
     const {data: plot, isPending, error} = useFetch("http://localhost:8000/plots/" + id);
+    let plotIsClaimed
 
     //simulate ethereum calls for now
     let fetchPlotStatus = true;
-    let plotIsMinted = false;
+    if (id%2 === 0){
+        plotIsClaimed = true;
+    }else{
+        plotIsClaimed = false;
+    }
 
     if (!fetchPlotStatus){
         return <section className="fetch-error">could not fetch the data from blockchain!</section>;
@@ -21,7 +27,7 @@ const PlotDetail = () => {
         <div>
             {error && <div className="fetch-error">could not fetch the data for that resource</div>}
             {isPending && <div>Loading...</div>}
-            {plotIsMinted ? "</> ": <PlotFree plot={plot} />}
+            {plotIsClaimed ? <PlotClaimed plot={plot}/>: <PlotFree plot={plot} />}
 
         </div>
      );

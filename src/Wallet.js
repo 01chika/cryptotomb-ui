4390627eff13fb455c4febce1e0c83740160be0f
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2021-08-24 19:29:20 -0400

    initialized wallet.js component and added prototype render

--- src/Wallet.js
+++ src/Wallet.js
@@ -1,7 +1,41 @@
+import Web3 from "web3";
+import { useState } from "react";
+
 const Wallet = () => {
+    const [metamaskStatus, setmetamaskStatus] = useState(false);
+    let account;
+    let accounts;
+
+    const handleClick = () => {
+        try{
+            accounts = window.ethereum.request({ method: 'eth_requestAccounts' });
+            account = accounts[0];
+            setmetamaskStatus(true)
+
+        }catch{
+            setmetamaskStatus(false)
+        }
+    }
+
     return ( 
         <section className="ct-wallet">
-            <h1>myCTWallet</h1>
+        <p>{console.log(metamaskStatus)}</p>
+
+        {metamaskStatus 
+        
+        ? 
+
+            <h1></h1> 
+        
+        
+        : 
+        
+            <div className="connect-wallet">
+                <img src={"/metamask-logo-horizontal.svg"} alt="metamask-logo" />
+                <h1>Connect your Metamask Wallet to cryptotomb to access this page !</h1>
+                <div onClick={handleClick} className="enable-metamask">Enable Metamask</div>
+            </div> }
+            
         </section>
      );
 }
